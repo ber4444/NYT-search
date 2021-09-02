06b1b01915e2eb5f1d10a8d47f5a899ce8873f44
ber4444 <ber4444@users.noreply.github.com>
2021-09-02 01:01:27 -0700

    fix

--- app/src/main/java/com/example/gabor/di/AppModule.kt
+++ app/src/main/java/com/example/gabor/di/AppModule.kt
@@ -10,7 +10,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
@@ -28,7 +27,6 @@ object AppModule {
     fun provideRetrofit(httpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(NewsApi.BASE_URL)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .client(httpClient)
             .build()
